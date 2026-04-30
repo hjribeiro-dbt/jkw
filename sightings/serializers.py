24fd533b7b44e09fefc336b7a87e1eb435256d83
Helder Ribeiro <helder.ribeiro@digital.trade.gov.uk>
2026-04-30 16:47:50 +0100

    feat:(api)-Add sightings endpoint

---
+++ sightings/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import JKWSighting
+
+
+class JKWSightingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JKWSighting
+        fields = [
+            "id",
+            "latitude",
+            "longitude",
+            "description",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
+
+    def validate_latitude(self, value):
+        if value < -90 or value > 90:
+            raise serializers.ValidationError("Latitude must be between -90 and 90.")
+        return value
+
+    def validate_longitude(self, value):
+        if value < -180 or value > 180:
+            raise serializers.ValidationError("Longitude must be between -180 and 180.")
+        return value
